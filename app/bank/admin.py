eed5f1542255c08ad6b09ea254fbd0147a08b019
geekwise-yuki-xiong <geekwise.yuki.xiong@gmail.com>
2019-11-23 00:32:48 +0000

    Added admin inline for branch model and one to one field for account and customer model

--- app/bank/admin.py
+++ app/bank/admin.py
@@ -3,8 +3,32 @@ from bank.models import Branch, Account, Customer, Product
 
 # Register your models here.
 admin.site.register((
-    Branch,
     Account,
     Customer,
     Product,
 ))
+
+class BranchInline(admin.StackedInline):
+    model = Branch
+
+class AccountInline(admin.StackedInline):
+    model = Account
+
+class CustomerInline(admin.StackedInline):
+    model = Customer
+
+class ProductInline(admin.StackedInline):
+    model = Product
+
+@admin.register(Branch)
+class AccountAdmin(admin.ModelAdmin):
+    inlines = [
+        AccountInline,
+    ]
+
+# @admin.register()
+# class AccountAdmin(admin.ModelAdmin):
+#     inlines = [
+#         AccountInline,
+#     ]
+
--- app/bank/models.py
+++ app/bank/models.py
@@ -6,16 +6,10 @@ class Branch(models.Model):
     class Meta:
         verbose_name_plural = 'branches'
     bank = models.CharField(max_length=300)
+    location = models.CharField(max_length=300)
 
     def __str__(self):
         return(f"{self.bank}")
-
-class Account(models.Model):
-    holder = models.CharField(max_length=300)
-    balance = models.DecimalField(max_digits=300, decimal_places=2)
-
-    def __str__(self):
-        return(f"Account: {self.holder} | Balance: ${self.balance}")
 
 class Customer(models.Model):
     name = models.CharField(max_length=300)
@@ -25,6 +19,21 @@ class Customer(models.Model):
 
     def __str__(self):
         return(f"{self.pk}: {self.name} | {self.email}")
+
+class Account(models.Model):
+    bank_partner = models.ForeignKey(
+        Branch,
+        on_delete=models.CASCADE,
+        related_name='bank_partner',
+    )
+    holder = models.OneToOneField(
+        Customer,
+        on_delete=models.CASCADE,
+    )
+    balance = models.DecimalField(max_digits=300, decimal_places=2)
+
+    def __str__(self):
+        return(f"Bank: {self.bank_partner.bank} | Account: {self.holder} | Balance: ${self.balance}")
 
 class Product(models.Model):
     options = (
